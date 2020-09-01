550e428cfa6f4db3942c66ac86690800b834d32d
wangjxian <wangjiaxian@shulie.io>
2020-09-01 15:37:22 +0800

    node

--- src/cn/com/stack/StaskSout.java
+++ src/cn/com/stack/StaskSout.java
@@ -1,5 +1,8 @@
 package cn.com.stack;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.Stack;
 import java.util.stream.Collectors;
 
@@ -15,6 +18,62 @@ public  abstract class StaskSout<T> {
         }
     }
 
+
+    private static class Node{
+       private int code;
+       private String name;
+
+        public Node(int code, String name) {
+            this.code = code;
+            this.name = name;
+        }
+
+        public int getCode() {
+            return code;
+        }
+
+        public void setCode(int code) {
+            this.code = code;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public String toString() {
+            return "Node{" +
+                    "code=" + code +
+                    ", name='" + name + '\'' +
+                    '}';
+        }
+    }
+
+
+    public static void main(String[] args) {
+        List<Node> nodes = new ArrayList<>();
+        nodes.add(new Node(1,"node1"));
+        nodes.add(new Node(2,"node2"));
+        nodes.add(new Node(3,"node3"));
+        nodes.add(new Node(3,"node4"));
+        nodes.add(new Node(4,"node5"));
+        nodes.add(new Node(5,"node6"));
+
+        Map<Integer, List<Node>> map1 = nodes.stream().collect(Collectors.groupingBy(Node::getCode));
+        soutNodeMap(map1);
+
+        Map<Integer, List<Node>> map2 = nodes.stream().collect(Collectors.groupingBy(Node::getCode, Collectors.mapping(obj -> obj, Collectors.toList())));
+        soutNodeMap(map2);
+    }
+
+    private static void soutNodeMap(Map<Integer, List<Node>> map) {
+        String collect = map.entrySet().stream().map(e -> e.getKey() + ":" + e.getValue().stream().map(Node::toString).collect(Collectors.joining(";","[","]"))).collect(Collectors.joining(","));
+        System.out.println(collect);
+    }
 
 
 }
